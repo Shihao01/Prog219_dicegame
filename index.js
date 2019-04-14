a84cc08501bfdba5dff0d3ceec084e9a4a6d55b1
angelinawainhouse <angelinawainhouse@users.noreply.github.com>
2019-04-13 20:58:07 -0700

    Add files via upload
    Updated dice game, AW

--- index.js
+++ index.js
@@ -1,83 +1,122 @@
-/**
-* Created by bcuser on 4/4/19.
-*/
-
-(function () {
-  // this function is strict...
-  "use strict";
-}());
+﻿/**
+ * Created by bcuser on 4/4/19.
+ angelina wainhouse
+ PROG219 HW 2
+ */
 
 
-function buttonClicked() {
-  var dice1;
-  var dice2;
+ (function () {
+   // this function is strict...
+   "use strict";
+ }());
+ console.log("index.js");
 
-  dice1 = Math.ceil(Math.random()* 6);
-  dice2 = Math.ceil(Math.random()* 6);
+ function buttonClicked() {
+   console.log("buttonClicked");
+   var dice1;
+   var dice2;
+   var balance = 5;
+   var turnCount = 0;
+   turnCount = turnCount +1;
 
-  switch (dice1){
-    case 1:
-    document.getElementById('image1').src='images/dice-1.jpg';
-    break;
+   dice1 = Math.ceil(Math.random()* 6);
+   dice2 = Math.ceil(Math.random()* 6);
 
-    case 2:
-    document.getElementById('image1').src='images/dice-2.jpg';
+   switch (dice1){
+     case 1:
+     document.getElementById('image1').src='images/dice-1.jpg';
+     if (dice1 == dice2 || dice1 + dice2 == 11 || dice1 + dice2 == 7) {
+       balance = balance + 1;
+     }
 
-    break;
+     else balance = balance - 1;
+     document.getElementById('balance').innerHTML = balance;
+     document.getElementById('turnCount').innerHTML = turnCount;
+     break;
 
-    case 3:
-    document.getElementById('image1').src='images/dice-3.jpg';
+     case 2:
+     document.getElementById('image1').src='images/dice-2.jpg';
+     if (dice1 == dice2 || dice1 + dice2 == 11 || dice1 + dice2 == 7) {
+       balance = balance + 1;
+     }
+     else balance = balance - 1;
+     document.getElementById('balance').innerHTML = balance;
+     document.getElementById('turnCount').innerHTML = turnCount;
+     break;
 
-    break;
+     case 3:
+     document.getElementById('image1').src='images/dice-3.jpg';
+     if (dice1 == dice2 || dice1 + dice2 == 11 || dice1 + dice2 == 7) {
+       balance = balance + 1;
+     }
+     else balance = balance - 1;
+     document.getElementById('balance').innerHTML = balance;
+     document.getElementById('turnCount').innerHTML = turnCount;
+     break;
 
-    case 4:
-    document.getElementById('image1').src='images/dice-4.jpg';
+     case 4:
+     document.getElementById('image1').src='images/dice-4.jpg';
+     if (dice1 == dice2 || dice1 + dice2 == 11 || dice1 + dice2 == 7) {
+       balance = balance + 1;
+     }
+     else balance = balance - 1;
+     document.getElementById('balance').innerHTML = balance;
+     document.getElementById('turnCount').innerHTML = turnCount;
+     break;
 
-    break;
+     case 5:
+     document.getElementById('image1').src='images/dice-5.jpg';
+     if (dice1 == dice2 || dice1 + dice2 == 11 || dice1 + dice2 == 7) {
+       balance = balance + 1;
+     }
+     else balance = balance - 1;
+     document.getElementById('balance').innerHTML = balance;
+     document.getElementById('turnCount').innerHTML = turnCount;
+     break;
 
-    case 5:
-    document.getElementById('image1').src='images/dice-5.jpg';
+     case 6:
+     document.getElementById('image1').src='images/dice-6.jpg';
+     if (dice1 == dice2 || dice1 + dice2 == 11 || dice1 + dice2 == 7) {
+       balance = balance + 1;
+     }
+     else balance = balance - 1;
+     document.getElementById('balance').innerHTML = balance;
+     document.getElementById('turnCount').innerHTML = turnCount;
+     break;
 
-    break;
+   }
 
-    case 6:
-    document.getElementById('image1').src='images/dice-6.jpg';
+   switch (dice2){
+     case 1:
+     document.getElementById('image2').src='images/dice-1.jpg';
+     break;
 
-    break;
+     case 2:
+     document.getElementById('image2').src='images/dice-2.jpg';
 
-  }
+     break;
 
-  switch (dice2){
-    case 1:
-    document.getElementById('image2').src='images/dice-1.jpg';
-    break;
+     case 3:
+     document.getElementById('image2').src='images/dice-3.jpg';
 
-    case 2:
-    document.getElementById('image2').src='images/dice-2.jpg';
+     break;
 
-    break;
+     case 4:
+     document.getElementById('image2').src='images/dice-4.jpg';
 
-    case 3:
-    document.getElementById('image2').src='images/dice-3.jpg';
+     break;
 
-    break;
+     case 5:
+     document.getElementById('image2').src='images/dice-5.jpg';
 
-    case 4:
-    document.getElementById('image2').src='images/dice-4.jpg';
+     break;
 
-    break;
+     case 6:
+     document.getElementById('image2').src='images/dice-6.jpg';
 
-    case 5:
-    document.getElementById('image2').src='images/dice-5.jpg';
+     break;
 
-    break;
-
-    case 6:
-    document.getElementById('image2').src='images/dice-6.jpg';
-
-    break;
-
-  }
+   }
 
 
-}
+ }
